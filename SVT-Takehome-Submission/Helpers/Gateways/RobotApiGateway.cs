3cc37880564641960b65abe93d48a6945878133e
Brian Ronstrom <brianr.emil1@gmail.com>
2022-05-05 08:23:42 -0400

    Updated robot api gateway to use environment variable.

--- SVT-Takehome-Submission/Helpers/Gateways/RobotApiGateway.cs
+++ SVT-Takehome-Submission/Helpers/Gateways/RobotApiGateway.cs
@@ -9,7 +9,7 @@ namespace SVT_Takehome_Submission.Helpers.Gateways
 {
     public class RobotApiGateway
     {
-        public List<Robot> Robots()
+        public static List<Robot> Robots()
         {
             HttpResponseMessage response = Get("robots");
             List<Robot> robots = null;
@@ -18,10 +18,10 @@ namespace SVT_Takehome_Submission.Helpers.Gateways
             return robots;
         }
 
-        private HttpResponseMessage Get(string path)
+        private static HttpResponseMessage Get(string path)
         {
             HttpClient client = new HttpClient();
-            HttpResponseMessage response = client.GetAsync("https://60c8ed887dafc90017ffbd56.mockapi.io/" + path).Result;
+            HttpResponseMessage response = client.GetAsync(Environment.GetEnvironmentVariable("ROBOT_API_URL") + path).Result;
 
             return response;
         }
