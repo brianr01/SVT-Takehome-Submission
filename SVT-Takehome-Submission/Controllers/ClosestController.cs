7b0a01609cac909fc20e471eb0d391bdbc2d82f9
Brian Ronstrom <brianr.emil1@gmail.com>
2022-05-05 14:23:47 -0400

    Added required functionality to closest endpoint.

--- SVT-Takehome-Submission/Controllers/ClosestController.cs
+++ SVT-Takehome-Submission/Controllers/ClosestController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SVT_Takehome_Submission.Helpers.Gateways;
 using SVT_Takehome_Submission.Models;
 using System;
 using System.Collections.Generic;
@@ -12,10 +13,58 @@ namespace SVT_Takehome_Submission.Controllers
     [ApiController]
     public class ClosestController : ControllerBase
     {
+        private static readonly int shortDistance = 10;
+
         [HttpPost]
         public IActionResult Post(Load load)
         {
-            return Ok(load);
+
+            List<Robot> robots = RobotApiGateway.Robots();
+
+            bool hasFoundCloseRobot = false;
+
+            Robot optimalRobot = null;
+            double? closestDistance = null;
+
+            foreach(var robot in robots)
+            {
+                double distance = GetDistance(robot.x, robot.y, load.x, load.y);
+
+                if (distance < shortDistance)
+                {
+                    hasFoundCloseRobot = true;
+                    if (optimalRobot.batteryLevel < robot.batteryLevel)
+                    {
+                        optimalRobot = robot;
+                        closestDistance = distance;
+                    }
+                    continue;
+                }
+
+                if (!hasFoundCloseRobot && distance < closestDistance || optimalRobot is null)
+                {
+                    optimalRobot = robot;
+                    closestDistance = distance;
+                }
+            }
+
+            OptimalRobotReturn robotReturn = new OptimalRobotReturn
+            {
+                robotId = Convert.ToInt32(optimalRobot.robotId),
+                distanceToGoal = closestDistance,
+                batteryLevel = optimalRobot.batteryLevel
+            };
+
+            return Ok(robotReturn);
+        }
+
+        private double GetDistance(double x1, double y1, double x2, double y2)
+        {
+            return Math.Sqrt(
+                Math.Pow(x1 - x2, 2)
+                +
+                Math.Pow(y1 - y2, 2)
+            );
         }
     }
 }
---
+++ SVT-Takehome-Submission/Models/optimalRobotReturn.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SVT_Takehome_Submission.Models
+{
+    public class OptimalRobotReturn
+    {
+        public int robotId { get; set; }
+        public double? distanceToGoal { get; set; }
+        public int batteryLevel { get; set; }
+    }
+
+}
