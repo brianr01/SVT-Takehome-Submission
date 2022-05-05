523cd67a9df53b226ab6151d7c2b260c03accc57
Brian Ronstrom <brianr.emil1@gmail.com>
2022-05-05 00:22:13 -0400

    Added controller for closest robot.

---
+++ SVT-Takehome-Submission/Controllers/ClosestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SVT_Takehome_Submission.Controllers
+{
+    [Route("api/robots/[controller]")]
+    [ApiController]
+    public class ClosestController : ControllerBase
+    {
+    }
+}
