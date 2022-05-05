849a043673226056bd1424336693511e74615a1c
Brian Ronstrom <brianr.emil1@gmail.com>
2022-05-05 00:37:17 -0400

    Added new model for a load (payload).

---
+++ SVT-Takehome-Submission/Models/Load.cs
@@ -0,0 +1,11 @@
+﻿
+namespace SVT_Takehome_Submission.Models
+{
+    public class Load
+    {
+        public int loadId { get; set; }
+        public int x { get; set; }
+        public int y { get; set; }
+    }
+
+}
