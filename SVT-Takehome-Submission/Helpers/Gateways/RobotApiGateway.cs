e6e8d3ead40aaeccfe2448528276cbb41a8e9447
Brian Ronstrom <brianr.emil1@gmail.com>
2022-05-05 01:02:22 -0400

    Added gate way for robots api.

---
+++ SVT-Takehome-Submission/Helpers/Gateways/RobotApiGateway.cs
@@ -0,0 +1,29 @@
+﻿using SVT_Takehome_Submission.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SVT_Takehome_Submission.Helpers.Gateways
+{
+    public class RobotApiGateway
+    {
+        public List<Robot> robots()
+        {
+            HttpResponseMessage response = get("robots");
+            List<Robot> robots = null;
+            robots = response.Content.ReadAsAsync<List<Robot>>().Result;
+
+            return robots;
+        }
+
+        private HttpResponseMessage get(string path)
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = client.GetAsync("https://60c8ed887dafc90017ffbd56.mockapi.io/" + path).Result;
+
+            return response;
+        }
+    }
+}
