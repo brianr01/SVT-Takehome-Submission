671dfc24fd76c0b3b468e2e7109bb53b71892a1e
Brian Ronstrom <brianr.emil1@gmail.com>
2022-05-05 01:01:13 -0400

    Add Robot model.

---
+++ SVT-Takehome-Submission/Models/Robot.cs
@@ -0,0 +1,11 @@
+﻿namespace SVT_Takehome_Submission.Models
+{
+    public class Robot
+    {
+        public string robotId { get; set; }
+        public int batteryLevel { get; set; }
+        public int y { get; set; }
+        public int x { get; set; }
+    }
+
+}
